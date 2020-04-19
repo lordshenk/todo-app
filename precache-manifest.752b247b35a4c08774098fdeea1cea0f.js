9bedab8f3a08aa57204644f893ae337cf5f1ea70
s1mple <duykhanh29091999@gmail.com>
2020-04-19 15:26:01 +0700

    Updates

---
+++ precache-manifest.752b247b35a4c08774098fdeea1cea0f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2503aab7845a2ba13adc7673bc03e7a8",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "ad8e7fd36b4245647811",
+    "url": "/todo-app/static/css/main.aa76f5fc.chunk.css"
+  },
+  {
+    "revision": "dc5ce885e40eb5f4d788",
+    "url": "/todo-app/static/js/2.0e731690.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.0e731690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad8e7fd36b4245647811",
+    "url": "/todo-app/static/js/main.4b8374f4.chunk.js"
+  },
+  {
+    "revision": "970872b872659dfe1663",
+    "url": "/todo-app/static/js/runtime-main.4fdb537a.js"
+  },
+  {
+    "revision": "734994c1b02ef19145ec7e92622d08eb",
+    "url": "/todo-app/static/media/double-check.734994c1.svg"
+  },
+  {
+    "revision": "4e8fb437645eca11022b2820d6c931b7",
+    "url": "/todo-app/static/media/tick-1.4e8fb437.svg"
+  }
+]);
